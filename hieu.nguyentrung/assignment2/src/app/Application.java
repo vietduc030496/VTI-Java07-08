0a58528d3ec0bf1194b41810e254ed5bc5eda5cd
HIEU Nguyen Trung2(VTI.Internship) <hieu.nguyentrung2@vti.vn>
2021-07-19 10:47:25 +0700

    Lastgit add .git add .!

--- hieu.nguyentrung/assignment2/src/app/Application.java
+++ hieu.nguyentrung/assignment2/src/app/Application.java
@@ -50,7 +50,6 @@ public class Application {
 				System.exit(0);
 			case 1:
 				Department dep1 = dep.DepartmentInput();
-<<<<<<< HEAD
 				try {
 					if (d.size() == 0) {
 						d.add(dep1);
@@ -70,9 +69,7 @@ public class Application {
 				} catch (ConcurrentModificationException e) {
 					d.add(dep1);
 				}
-=======
 				d.add(dep1);
->>>>>>> 753945ae11b213a944391815f143416bbbffe633
 				break;
 			case 2:
 				dep.DisplayDepartment(d);
@@ -114,12 +111,8 @@ public class Application {
 				emp.DeleteEmployee(d);
 				break;
 			case 10:
-<<<<<<< HEAD
 				dName = scanner.nextLine();
 				dep.UpdateDepartment(d, dName);
-=======
-				dep.
->>>>>>> 753945ae11b213a944391815f143416bbbffe633
 				break;
 //			case 11:
 //				break;
