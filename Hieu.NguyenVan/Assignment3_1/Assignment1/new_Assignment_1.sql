a40a3a84e992d8a1606e5f22a5b07ae0c1c3baee
VanHieu <hieuhamhocpro@gmail.com>
2021-07-20 16:42:48 +0700

    done sql assignment 1,2 ,3

--- Hieu.NguyenVan/Assignment3_1/Assignment1/new_Assignment_1.sql
+++ Hieu.NguyenVan/Assignment3_1/Assignment1/new_Assignment_1.sql
@@ -1,4 +1,4 @@
-create database  company;
+create database if not exists  company;
 use company ;
 
 create table if not exists  department(
@@ -31,4 +31,64 @@ create table if not exists group_table   (
     CreateDate date 
     
 );
+create table if not exists GroupAccount(
+	GroupID int auto_increment primary key,
+    AccountID int,
+    foreign key (AccountID)  references account(AccountID),
+    JoinDate date
+);
+create table if not exists TypeQuestion(
+	TypeID int auto_increment primary key,
+    TypeName varchar(225)
+);
+
+create table if not exists CategoryQuestion(
+	CategoryID int auto_increment primary key,
+    CategoryName varchar(225) 
+);
+create table if not exists Question(
+	QuestionID int auto_increment primary key,
+    Content varchar(225),
+    CategoryID int,
+    foreign key (CategoryID) references CategoryQuestion(CategoryID),
+    TypeID int ,
+    foreign key (TypeID) references TypeQuestion(TypeID),
+    CreatorID int ,
+    foreign key (CreatorID) references account(AccountID),
+    CreateDate date
+    
+);
+create table if not exists Answer(
+	 AnswerID int auto_increment primary key ,
+     Content varchar(225),
+     QuestionID int,
+     foreign key (QuestionID) references Question(QuestionID),
+     isCorrect boolean
+);
+create table if not exists Exam(
+	ExamID int auto_increment primary key,
+    Code int(10) ,
+    Title varchar (225),
+    CategoryID int,
+    foreign key (categoryID) references CategoryQuestion(CategoryID),
+    Duration int(10),
+    CreatorID int,
+    foreign key (CreatorID) references account(AccountID),
+    CreateDate date
+    
+);
+create table if not exists ExamQuestion(
+	ExamID int ,
+    foreign key (ExamID) references Exam(ExamID),
+    QuestionID int ,
+    foreign key(QuestionID) references Question(QuestionID)
+);
+
+
+
+
+
+
+
+
 
