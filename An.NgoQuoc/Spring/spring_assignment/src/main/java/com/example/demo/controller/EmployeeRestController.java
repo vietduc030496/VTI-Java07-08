9e7e7114452c7d3f4a304f6bd26e42d6b6311d13
ngoquocan <ngoquocan161199@gmail.com>
2021-08-10 14:09:52 +0700

    update spring ass1

--- An.NgoQuoc/Spring/spring_assignment/src/main/java/com/example/demo/controller/EmployeeRestController.java
+++ An.NgoQuoc/Spring/spring_assignment/src/main/java/com/example/demo/controller/EmployeeRestController.java
@@ -9,47 +9,49 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.EmployeeDto;
 import com.example.demo.service.EmployeeService;
 
 @RestController
-public class EmployeeController {
+@RequestMapping("/api/employee")
+public class EmployeeRestController {
 
 	@Autowired
 	private EmployeeService employeeService;
 	
-	@PostMapping("/employee")
+	@PostMapping()
 	public String createEmployee(@RequestBody EmployeeDto employeeDto) {
 		employeeService.saveOrUpdate(employeeDto);
 		return "success";
 	}
 	
-	@GetMapping("/employee")
+	@GetMapping()
 	public List<EmployeeDto> getAll() {
 		return employeeService.findAll();
 	}
 	
-	@GetMapping("/employee/{id}")
+	@GetMapping("/{id}")
 	public EmployeeDto getById(@PathVariable("id") Long id) {
 		return employeeService.findOneById(id);
 	}
 	
-	@PutMapping("/employee/{id}")
+	@PutMapping("/{id}")
 	public String updateEmployee(@RequestBody EmployeeDto employeeDto,@PathVariable("id") Long id ) {
 		employeeDto.setId(id);
 		employeeService.saveOrUpdate(employeeDto);
 		return "success";
 	}
 	
-	@DeleteMapping("/employee/{id}")
+	@DeleteMapping("/{id}")
 	public String deleteById(@PathVariable("id") Long id ) {
 		employeeService.deleteById(id);
 		return "success";
 	}
 	
-	@DeleteMapping("/employee")
+	@DeleteMapping()
 	public String deleteEmployee(@RequestBody EmployeeDto employeeDto) {
 		employeeService.delete(employeeDto);
 		return "success";
