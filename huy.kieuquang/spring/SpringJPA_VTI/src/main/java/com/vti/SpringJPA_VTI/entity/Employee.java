08e88addfe67157cb44dc037e0f1b43f6fcc676b
kyowanghwe <kieuquanghuy1999@gmail.com>
2021-08-11 22:15:48 +0700

    thymleaf update

--- huy.kieuquang/spring/SpringJPA_VTI/src/main/java/com/vti/SpringJPA_VTI/entity/Employee.java
+++ huy.kieuquang/spring/SpringJPA_VTI/src/main/java/com/vti/SpringJPA_VTI/entity/Employee.java
@@ -15,6 +15,20 @@ public class Employee {
 	private String lastName;
 	private String email;
 	private String phone;
+	
+	public Employee() {
+		
+	}
+
+	
+	public Employee(int id, String firstName, String lastName, String email, String phone) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phone = phone;
+	}
+
 
 	public int getId() {
 		return id;
