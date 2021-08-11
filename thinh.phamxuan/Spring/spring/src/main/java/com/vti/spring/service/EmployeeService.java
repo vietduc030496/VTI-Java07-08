5ef1cf99f1476c0f7aff4c60846a6b6d26693f3f
phamthinh123 <thinoi987654321@gmail.com>
2021-08-12 01:20:30 +0700

    thymeleaf-assignment

--- thinh.phamxuan/Spring/spring/src/main/java/com/vti/spring/service/EmployeeService.java
+++ thinh.phamxuan/Spring/spring/src/main/java/com/vti/spring/service/EmployeeService.java
@@ -6,11 +6,10 @@ import java.util.Optional;
 import com.vti.spring.entity.Employee;
 
 public interface EmployeeService {
-	public void save(Employee employee);
-	public void update(int id,Employee employee);
-	public List<Employee> getEmployee();
-	public Optional<Employee> getEmployeeById(int id);
-	public void deleteEmployeeById(int id);
+	public void saveEmployee(Employee employee);
+	public List<Employee> getAllEmployee();
+	public Optional<Employee> findEmployeeById(int id);
+	public void deleteEmployee(int id);
 	
 
 }
